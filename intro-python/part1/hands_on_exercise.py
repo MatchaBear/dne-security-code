cd5ad5a0fe7aa372bf5374fffa056bd7c5c4ac70
MatchaBear <matchabear@users.noreply.github.com>
2021-01-28 16:30:41 +0800

    Update hands_on_exercise.py

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -15,21 +15,34 @@ print("pi is a {} with a value of {}".format(type(pi),pi))
 i = random.randint(0, 100)
 #while i<50 or i>50:
 #    print(i)
-
-
+print(i)
+if i < 50:
+    print("'i' is less than 50")
+elif i == 50:
+    print("'i' is equal to 50")
+else:
+    print("'i' is greater than 50")
 
 # TODO: Write a conditional that prints the color of the picked fruit
 picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
-
+print(picked_fruit)
+if picked_fruit == 'orange':
+    print("orange")
+elif picked_fruit == 'strawberry':
+    print("red")
+else:
+    print("yellow")
 
 # TODO: Write a function that multiplies two numbers and returns the result
 # Define the function here.
-
+def multiply(a,b) -> int:
+    result = a*b
+    return result
 
 # TODO: Now call the function a few times to calculate the following answers
 
-print("12 x 96 =", )
+print("12 x 96 =", multiply(12,96))
 
-print("48 x 17 =", )
+print("48 x 17 =", multiply(48,17))
 
-print("196523 x 87323 =", )
+print("196523 x 87323 =", multiply(196523,87323))
