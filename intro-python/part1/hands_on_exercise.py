d3058f8091df587e1853b29e1e1c12d0b45f723b
MatchaBear <matchabear@users.noreply.github.com>
2021-01-28 11:35:11 +0800

    noob participant
    i am seeing the best practice

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -7,12 +7,16 @@ import random
 
 # TODO: Write a print statement that displays both the type and value of `pi`
 pi = math.pi
-print(pi)
+#print(pi)
+
+print("pi is a {} with a value of {}".format(type(pi),pi))
 
 # TODO: Write a conditional to print out if `i` is less than or greater than 50
 i = random.randint(0, 100)
-while i<50 or i>50:
-    print(i)
+#while i<50 or i>50:
+#    print(i)
+
+
 
 # TODO: Write a conditional that prints the color of the picked fruit
 picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
